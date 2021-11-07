adb4ac4a4d72a430bc7ce0400b3d18cd0594b7b6
JuankmiloGh <juancamiloh7@gmail.com>
2021-11-07 16:49:50 -0500

    'BackendV4.0'

--- src/main/java/com/Ciclo3Backend/Ciclo3Backend/Controller/MachineController.java
+++ src/main/java/com/Ciclo3Backend/Ciclo3Backend/Controller/MachineController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Manager
  */
 @RestController
-@RequestMapping("/api/Gymmachine")
+@RequestMapping("/api/Machine")
 @CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class MachineController {
     @Autowired
